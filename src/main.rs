5da1c4148e202cf891432687ccb125cdd1a485fa
David Terrell <dbt@meat.net>
2019-12-03 23:28:12 -0800

    first problem part 2 solved.

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,26 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn advent01a_fuel(mass: u32) -> u32 {
-    return (mass/3)-2;
+fn advent01a_fuel(mass: i32) -> i32 {
+    let val = (mass/3)-2;
+    if val <= 0 {
+        return 0;
+    }
+    if val < 9 {
+        return val;
+    }
+    let addl = advent01a_fuel(val);
+    return val + addl;
+}
+
+fn advent01a() -> Result<String, Box<dyn Error>> {
+    let lines = read_lines("a01-input")?;
+    let mut total: i32 = 0;
+    for line in lines {
+        let val = line?.parse::<i32>()?;
+        total += advent01a_fuel(val);
+    }
+    Ok(total.to_string())
 }
 
 #[cfg(test)]
@@ -26,17 +44,7 @@ mod tests {
     fn test_advent01a_fuel() {
         assert_eq!(advent01a_fuel(12), 2);
         assert_eq!(advent01a_fuel(14), 2);
-        assert_eq!(advent01a_fuel(1969), 654);
-        assert_eq!(advent01a_fuel(100756), 33583);
+        assert_eq!(advent01a_fuel(1969), 966);
+        assert_eq!(advent01a_fuel(100756), 50346);
     }
-}
-
-fn advent01a() -> Result<String, Box<dyn Error>> {
-    let lines = read_lines("a01-input")?;
-    let mut total: u32 = 0;
-    for line in lines {
-        let val = line?.parse::<u32>()?;
-        total += advent01a_fuel(val);
-    }
-    Ok(total.to_string())
 }
