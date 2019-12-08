30d809ed622ecc74eff6f38c550296b89eeae120
David Terrell <dbt@meat.net>
2019-12-07 20:34:48 -0800

    prep for day 8

---
+++ src/day08.rs
@@ -0,0 +1,15 @@
+use crate::utils::{self, Result};
+use crate::computer;
+
+pub fn part1() -> Result<String> {
+    return Ok("hello".to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_() {
+    }
+}
