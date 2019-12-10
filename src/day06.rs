5759e150bc3f2db85fa07be27d207a2973e3e009
David Terrell <dbt@meat.net>
2019-12-09 22:54:51 -0800

    reasonable day 6 impl

---
+++ src/day06.rs
@@ -0,0 +1,69 @@
+
+use std::path::Path;
+use crate::utils::{read_lines,Result};
+use std::collections::HashMap;
+use std::fs;
+
+pub fn part1() -> Result<String> {
+    let all = fs::read_to_string("a06-input")?;
+    let mut queue = vec!["COM"];
+    let mut next_queue: Vec<&str> = Vec::new();
+    let mut dist = 0;
+    let mut total = 0;
+    while !queue.is_empty() {
+        dist += 1;
+        for line in all.split("\n") {
+            if line.len() < 1 {
+                break;
+            }
+            let mut spl = line.split(")");
+            let inner = spl.next().ok_or(format!("invalid {}", line))?;
+            let outer = spl.next().ok_or(format!("invalid {}", line))?;
+            if queue.contains(&inner) {
+                total += dist;
+                next_queue.push(outer);
+            }
+        }
+        queue = next_queue;
+        next_queue = vec![];
+    }
+    Ok(total.to_string())
+}
+
+pub fn part2() -> Result<String> {
+    let all = fs::read_to_string("a06-input")?;
+    let all_lines = all.split("\n");
+    let mut parents: HashMap<&str, &str> = HashMap::new();
+    for line in all_lines {
+        if line.len() < 1 {
+            break;
+        }
+        let mut spl = line.split(")");
+        let inner = spl.next().ok_or(format!("invalid {}", line))?;
+        let outer = spl.next().ok_or(format!("invalid {}", line))?;
+        parents.insert(outer, inner);
+    }
+    let mut counter = -1;
+    let mut ynext = Some(&"YOU");
+    let mut snext = Some(&"SAN");
+    let mut ydist: HashMap<&str, i32> = HashMap::new();
+    let mut sdist: HashMap<&str, i32> = HashMap::new();
+    while ynext.is_some() && snext.is_some() {
+        if let Some(you) = ynext {
+            ydist.insert(you, counter);
+            if let Some(x) = sdist.get(you) {
+                return Ok((x+counter).to_string());
+            }
+            ynext = parents.get(you);
+        }
+        if let Some(san) = snext {
+            sdist.insert(san, counter);
+            if let Some(x) = ydist.get(san) {
+                return Ok((x+counter).to_string());
+            }
+            snext = parents.get(san);
+        }
+        counter += 1;
+    }
+    Err("no matches found")?
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use advent2019::day02;
 use advent2019::day03;
 use advent2019::day04;
 use advent2019::day05;
+use advent2019::day06;
 use advent2019::day07;
 use advent2019::day08;
 
@@ -21,6 +22,8 @@ fn main() -> Result<()> {
     println!("day  4 part 2: {}", day04::part2()?);
     println!("day  5 part 1: {}", day05::part1()?);
     println!("day  5 part 2: {}", day05::part2()?);
+    println!("day  6 part 1: {}", day06::part1()?);
+    println!("day  6 part 2: {}", day06::part2()?);
     println!("day  7 part 1: {}", day07::part1()?);
     println!("day  8 part 1: {}", day08::part1()?);
     println!("day  8 part 2: \n{}", day08::part2()?);
